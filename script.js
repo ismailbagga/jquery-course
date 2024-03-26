5080579c8192b03e3373313aa6c873a73e12de4b
imdev <ismailbak7@gmail.com>
2024-03-26 10:24:49 +0000

    Adding the lightbox

--- script.js
+++ script.js
@@ -1,1 +1,29 @@
-console.log($)
+
+const btnEL = document.querySelector('.toggle')
+
+// var const let => 
+let isShown = false 
+const  onClickHandler = (event) => {
+    // Element is hidden 
+    if (isShown === false )  {
+        $('#first-box').show()
+        isShown = true 
+    }
+    else {
+        $('#first-box').hide()
+        isShown = false
+    }
+
+}
+
+
+
+setTimeout(()=> {
+
+
+    $('.lightbox').show()
+    // $('.lightbox').fadeIn(3000)
+
+} , 5000)
+// btnEL.onClick = onClickHandler
+btnEL.addEventListener('click',onClickHandler)
